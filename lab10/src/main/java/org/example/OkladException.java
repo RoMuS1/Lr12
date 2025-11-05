0c2e205a65026d7c16aaabffd462989af6f7ebd8
RoMuS1 <153280344+RoMuS1@users.noreply.github.com>
2025-11-05 16:28:11 +0300

    Delete lab10 directory

--- lab10/src/main/java/org/example/OkladException.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class OkladException extends Exception {
-    public OkladException(double oklad) {
-        super("Невозможно создать сотрудника – указан отрицательный оклад: " + oklad);
-    }
-}
--- lab10/src/main/java/org/example/PremiyaException.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class PremiyaException extends Exception {
-    public PremiyaException(String message) {
-        super(message);
-    }
-}
